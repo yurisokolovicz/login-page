41567252e14d76244727dc3304c14f31dcb6b29d
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-21 15:38:44 -0300

    Using the useEffect() Hook
    useEffect is a React hook that lets you run additional code on functional components, such as making an API call, manipulating the DOM, updating state, or subscribing and unsubscribing to events. In the presented code, useEffect is used to check if the user is logged in to the site and update the component state accordingly. It runs only on the first rendering of the component, because the second argument (array of dependencies) is an empty array, which means it doesn't depend on any specific state or property to run again. useEffect uses localStorage to store user login state and update component state when needed. Additionally, the component state is updated using the setIsLoggedIn() function, which is provided by useState().

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    setIsLoggedIn(true);
-  };
+    useEffect(() => {
+        const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
 
-  const logoutHandler = () => {
-    setIsLoggedIn(false);
-  };
+        if (storedUserLoggedInInformation === '1') {
+            setIsLoggedIn(true);
+        }
+    }, []); // it will run just in the first time the webpage is uploaded, not infinite loop, the dependencies are not changing.
 
-  return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
-    </React.Fragment>
-  );
+    const loginHandler = (email, password) => {
+        // We should of course check email and password
+        // But it's just a dummy/ demo anyways
+        localStorage.setItem('isLoogedIn', '1'); // 0: not logged; 1 = logged
+        setIsLoggedIn(true);
+    };
+
+    const logoutHandler = () => {
+        localStorage.removeItem('isLoggedIn');
+        setIsLoggedIn(false);
+    };
+
+    return (
+        <React.Fragment>
+            <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+            <main>
+                {!isLoggedIn && <Login onLogin={loginHandler} />}
+                {isLoggedIn && <Home onLogout={logoutHandler} />}
+            </main>
+        </React.Fragment>
+    );
 }
 
 export default App;
