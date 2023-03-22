57b2c8bbe82aa23f4230667fe744f91776fb4dc4
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-22 17:08:50 -0300

    Build version finished

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,7 +8,7 @@ const Home = props => {
     return (
         <Card className={classes.home}>
             <h1>Welcome back!</h1>
-            <Button onClick={props.onLogout}>Logout</Button>
+            {/* <Button onClick={props.onLogout}>Logout</Button> */}
         </Card>
     );
 };
